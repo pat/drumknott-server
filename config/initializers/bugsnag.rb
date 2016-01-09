b444968a789aa696bbec9d503e64af7f1f1cf485
Pat Allan <pat@freelancing-gods.com>
2016-01-09 15:44:28 +1100

    stripe, bugsnag, dotenv gems.

---
+++ config/initializers/bugsnag.rb
@@ -0,0 +1,5 @@
+Bugsnag.configure do |config|
+  config.api_key               = ENV['BUGSNAG_API_KEY']
+  config.notify_release_stages = ["production", "staging"]
+  config.release_stage         = Rails.env
+end
