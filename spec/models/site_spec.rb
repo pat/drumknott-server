bcc2fbffa993479c4908eebb5eeeea161c196404
Pat Allan <pat@freelancing-gods.com>
2016-01-13 19:22:55 +1100

    Validation for site names.

---
+++ spec/models/site_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Site, :type => :model do
+  describe '#valid?' do
+    it 'only allows names with lowercase letters, numbers, underscores and dashes' do
+      site = Site.new :name => 'foo'
+      site.valid?
+      expect(site.errors[:name].length).to eq(0)
+
+      site = Site.new :name => 'foo '
+      site.valid?
+      expect(site.errors[:name].length).to eq(1)
+
+      site = Site.new :name => 'Foo'
+      site.valid?
+      expect(site.errors[:name].length).to eq(1)
+
+      site = Site.new :name => 'foo-'
+      site.valid?
+      expect(site.errors[:name].length).to eq(0)
+
+      site = Site.new :name => 'foo_'
+      site.valid?
+      expect(site.errors[:name].length).to eq(0)
+
+      site = Site.new :name => 'foo_bar'
+      site.valid?
+      expect(site.errors[:name].length).to eq(0)
+
+      site = Site.new :name => 'foo$bar'
+      site.valid?
+      expect(site.errors[:name].length).to eq(1)
+    end
+  end
+end
