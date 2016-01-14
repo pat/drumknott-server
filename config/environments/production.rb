969347cfb8f3ccd414777654c5c57a7be408d2b4
Pat Allan <pat@freelancing-gods.com>
2016-01-14 22:02:53 +1100

    Set host for production emails.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {:host => 'drumknottsearch.com'}
   ActionMailer::Base.smtp_settings = {
     :port =>           '587',
     :address =>        'smtp.mandrillapp.com',
