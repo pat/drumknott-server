ecb50651c88448c2caaf66ce2d4b09b9f7b59876
Pat Allan <pat@freelancing-gods.com>
2016-01-14 23:55:53 +1100

    Note currency, tweak footer message order.

--- app/lib/invoice_as_pdf.rb
+++ app/lib/invoice_as_pdf.rb
@@ -26,8 +26,11 @@ Invoice From:
     move_down 10
     create_body
     move_down 10
-    text "Total amount listed includes 10% GST." if invoice.user.country == 'AU'
     text "#{ENV['INVOICE_REFERENCE_LABEL']}: #{ENV['INVOICE_REFERENCE']}"
+    text "All dollar values listed are in #{invoice.data['currency'].upcase}."
+    if invoice.user.country == 'AU'
+      text "Total amount listed includes 10% GST for Australian customers."
+    end
     text "For questions, support and feedback, please email <link href=\"mailto:hello@drumknottsearch.com\">hello@drumknottsearch.com</link>.",
       :inline_format => true
   end
