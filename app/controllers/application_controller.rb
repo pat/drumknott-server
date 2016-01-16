44f3efb9c65a06157a0ba8a7c04150c80d3cac92
Pat Allan <pat@freelancing-gods.com>
2016-01-16 15:41:22 +1100

    Add country field when signing up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   expose(:section) { 'home' }
+
+  before_filter :configure_permitted_parameters, :if => :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :country
+  end
 end
