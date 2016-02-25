94c694e38fe967911820a8653ad33761340a81a2
Pat Allan <pat@freelancing-gods.com>
2016-02-25 17:58:33 +1100

    Switching from Mandrill to Postmark for email.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,12 +65,13 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = {:host => 'drumknottsearch.com'}
   ActionMailer::Base.smtp_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
-    :domain =>         'heroku.com',
-    :authentication => :plain
+    :port                 => '25',
+    :address              => ENV['POSTMARK_SMTP_SERVER'],
+    :user_name            => ENV['POSTMARK_API_TOKEN'],
+    :password             => ENV['POSTMARK_API_TOKEN'],
+    :domain               => 'drumknottsearch.com',
+    :authentication       => :cram_md5,
+    :enable_starttls_auto => true
   }
   ActionMailer::Base.delivery_method = :smtp
 
