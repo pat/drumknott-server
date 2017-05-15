9e1bb6baea5b9c1b8bff94e8012b340c33d675cd
Pat Allan <pat@freelancing-gods.com>
2017-05-15 20:56:31 +1000

    Update syntax for Devise 4.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :country
+    devise_parameter_sanitizer.permit :sign_up, :keys => [:country]
   end
 end
