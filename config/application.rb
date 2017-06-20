71a272d287ea9739c49c3fe983e63da781a62b5b
Pat Allan <pat@freelancing-gods.com>
2017-06-20 10:12:58 +1000

    Update deprecated Rails settings.

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,6 @@ module Drumknott
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
 
     config.middleware.use Rack::Pratchett
     config.middleware.insert 0, Rack::Rewrite do
