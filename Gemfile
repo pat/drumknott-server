b303ac9f5df163abab5a6c8f506805a10725fef8
Pat Allan <pat@freelancing-gods.com>
2019-08-21 23:07:05 +1000

    Upgrade to Rails v6.0.

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ source "https://rubygems.org"
 ruby "2.6.3"
 
 # rails
-gem "rails", "5.2.3"
+gem "rails", "6.0.0"
 
 # core
 gem "pg",      "~> 1.0"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,10 +18,11 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=172800"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -53,6 +54,6 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 end
