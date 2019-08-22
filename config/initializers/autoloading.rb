31b7575d4b4ee80f0f9938d0d6fbbacca4aaf983
Pat Allan <pat@freelancing-gods.com>
2019-08-22 14:33:31 +1000

    Ensure app's indices path is a string.

--- config/initializers/autoloading.rb
+++ config/initializers/autoloading.rb
@@ -2,5 +2,5 @@
 
 # To avoid Zeitwerk's expectations on index definitions.
 ActiveSupport::Dependencies.autoload_paths.delete(
-  Rails.root.join("app", "indices")
+  Rails.root.join("app", "indices").to_s
 )
