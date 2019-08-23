19a197f283a3bb035f9b749a2c480536143a91d0
Pat Allan <pat@freelancing-gods.com>
2019-08-23 12:14:48 +1000

    Use TS-provided Zeitwerk fix.

--- config/initializers/autoloading.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# To avoid Zeitwerk's expectations on index definitions.
-ActiveSupport::Dependencies.autoload_paths.delete(
-  Rails.root.join("app", "indices").to_s
-)
