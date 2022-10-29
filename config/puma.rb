21b7eb112b0f33e64c4e4417b3e0eb695ad7c91c
Pat Allan <pat@freelancing-gods.com>
2022-10-30 00:04:44 +1100

    build: update Puma configuration.

--- config/puma.rb
+++ config/puma.rb
@@ -4,16 +4,13 @@ workers Integer(ENV.fetch("WEB_CONCURRENCY", 2))
 threads_count = Integer(ENV.fetch("MAX_THREADS", 5))
 threads threads_count, threads_count
 
-preload_app!
+# Specifies the `worker_timeout` threshold that Puma will use to wait before
+# terminating a worker in development environments.
+#
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-rackup      DefaultRackup
 port        Integer(ENV.fetch("PORT", 3000))
 environment ENV.fetch("RACK_ENV", "development")
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+pidfile     ENV.fetch("PIDFILE", "tmp/pids/server.pid")
 
 plugin :tmp_restart
